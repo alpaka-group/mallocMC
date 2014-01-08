411845d4ea78f09b8826f2c7372a2e4bd71ab1bd
Axel Huebl <axel.huebl@web.de>
2014-01-08 16:50:10 +0100

    Add a version.hpp file
    Can be read by CMake or in user code to determine the version
    of the library.

---
+++ src/include/scatteralloc/version.hpp
@@ -0,0 +1,44 @@
+/*
+  ScatterAlloc: Massively Parallel Dynamic Memory Allocation for the GPU.
+  http://www.icg.tugraz.at/project/mvp
+
+  Copyright (C) 2012 Institute for Computer Graphics and Vision,
+                     Graz University of Technology
+  Copyright (C) 2014 Institute of Radiation Physics,
+                     Helmholtz-Zentrum Dresden - Rossendorf
+
+  Author(s):  Markus Steinberger - steinberger ( at ) icg.tugraz.at
+              Rene Widera - r.widera ( at ) hzdr.de
+              Axel Huebl - a.huebl ( at ) hzdr.de
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
+#pragma once
+
+/** the ScatterAlloc version: major API changes should be reflected here */
+#define SCATTERALLOC_VERSION_MAJOR 1
+#define SCATTERALLOC_VERSION_MINOR 0
+#define SCATTERALLOC_VERSION_PATCH 2
+
+/** the ScatterAlloc flavor is used to differenciate the releases
+ *  of the Computational Radiation Physics group (crp) from the original
+ *  release of the TU Graz group (mvp)
+ *  This should be useful to avoid versioning conflicts */
+#define SCATTERALLOC_FLAVOR "crp"
