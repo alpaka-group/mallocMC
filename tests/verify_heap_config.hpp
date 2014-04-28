0d1578b2a867e243ecfa385ab7abe3e49829160f
Carlchristian Eckert <c.eckert@hzdr.de>
2014-04-28 13:33:44 +0200

    add a file for heap verification

---
+++ tests/verify_heap_config.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <boost/mpl/int.hpp>
+#include <boost/mpl/bool.hpp>
+
+// basic files for PolicyMalloc
+#include "src/include/scatteralloc/policy_malloc_overwrites.hpp"
+#include "src/include/scatteralloc/policy_malloc_hostclass.hpp"
+
+// Load all available policies for PolicyMalloc
+#include "src/include/scatteralloc/CreationPolicies.hpp"
+#include "src/include/scatteralloc/DistributionPolicies.hpp"
+#include "src/include/scatteralloc/OOMPolicies.hpp"
+#include "src/include/scatteralloc/ReservePoolPolicies.hpp"
+#include "src/include/scatteralloc/AlignmentPolicies.hpp"
+    
+
+// configurate the CreationPolicy "Scatter"
+struct ScatterConfig{
+    typedef boost::mpl::int_<4096>  pagesize;
+    typedef boost::mpl::int_<8>     accessblocks;
+    typedef boost::mpl::int_<16>    regionsize;
+    typedef boost::mpl::int_<2>     wastefactor;
+    typedef boost::mpl::bool_<false> resetfreedpages;
+};
+
+struct ScatterHashParams{
+    typedef boost::mpl::int_<38183> hashingK;
+    typedef boost::mpl::int_<17497> hashingDistMP;
+    typedef boost::mpl::int_<1>     hashingDistWP;
+    typedef boost::mpl::int_<1>     hashingDistWPRel;
+};
+
+// configure the DistributionPolicy "XMallocSIMD"
+struct DistributionConfig{
+  typedef ScatterConfig::pagesize pagesize;
+};
+
+// configure the AlignmentPolicy "Shrink"
+struct AlignmentConfig{
+  typedef boost::mpl::int_<16> dataAlignment;
+};
+
+// Define a new allocator and call it ScatterAllocator
+// which resembles the behaviour of ScatterAlloc
+typedef PolicyMalloc::PolicyAllocator< 
+  PolicyMalloc::CreationPolicies::Scatter<ScatterConfig,ScatterHashParams>,
+  PolicyMalloc::DistributionPolicies::XMallocSIMD<DistributionConfig>,
+  PolicyMalloc::OOMPolicies::ReturnNull,
+  PolicyMalloc::ReservePoolPolicies::SimpleCudaMalloc,
+  PolicyMalloc::AlignmentPolicies::Shrink<AlignmentConfig>
+  > ScatterAllocator;
