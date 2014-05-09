9b0fee73680a808c9306e2493a761232d9c3a3de
Carlchristian Eckert <c.eckert@hzdr.de>
2014-05-09 13:50:44 +0200

    Add install and uninstall targets
     - close #4
     - also adds a file "scatteralloc.hpp" that contains all necessary
       includes

---
+++ src/include/scatteralloc/scatteralloc.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+// generic stuff
+#include "version.hpp"
+
+// core functionality
+#include "policy_malloc_overwrites.hpp"
+#include "policy_malloc_hostclass.hpp"
+
+//all the policies
+#include "CreationPolicies.hpp"
+#include "DistributionPolicies.hpp"
+#include "ReservePoolPolicies.hpp"
+#include "AlignmentPolicies.hpp"
+#include "OOMPolicies.hpp"
